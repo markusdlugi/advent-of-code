f1ba162d2d607f40c0977ec36edd51e7a1c33ff6
Markus Dlugi <dlugi.markus@gmail.com>
2024-12-14 13:31:18 +0100

    Formalize day 14 using CRT

--- aoc-2024/day14.py
+++ aoc-2024/day14.py
@@ -11,11 +11,31 @@ width = 101
 height = 103
 
 
+def extended_euclid(a, b):
+    if b == 0:
+        return a, 1, 0
+    d, s, t = extended_euclid(b, a % b)
+    return d, t, s - (a // b) * t
+
+
+def chinese_remainder(modulos):
+    M = 1
+    for m, a in modulos:
+        M *= m
+
+    x = 0
+    for m, a in modulos:
+        Mi = M // m
+        x += extended_euclid(m, Mi)[2] * Mi * a
+
+    return x + (-1 * (x // M) * M) if x < 0 else x
+
+
 def print_robots(robots, seconds):
     print(f"After {seconds} seconds:")
     for r in range(width):
         for c in range(height):
-            if any(robot.px == r and robot.py == c for robot in robots):
+            if any(robot.py == r and robot.px == c for robot in robots):
                 print("#", end="")
             else:
                 print(" ", end="")
@@ -40,27 +60,47 @@ def calculate_safety_factor(robots):
     return reduce(mul, quadrants.values())
 
 
-def simulate_robots(robots, search_for_tree=True):
-    safety_factor = 0
-    for i in range(8200):
-        new_robots = []
-        for robot in robots:
-            px = (robot.px + robot.vx) % width if robot.px + robot.vx >= 0 else width + robot.px + robot.vx
-            py = (robot.py + robot.vy) % height if robot.py + robot.vy >= 0 else height + robot.py + robot.vy
-            new_robots.append(robot._replace(px=px, py=py))
-        robots = new_robots
-        if i == 99:
-            safety_factor = calculate_safety_factor(robots)
-            if not search_for_tree:
-                break
-        if i > 100 and (i - 114) % 103 == 0:
-            print_robots(robots, i + 1)
+def calculate_positions(robots, second):
+    new_robots = []
+    for robot in robots:
+        px = (robot.px + second * robot.vx) % width
+        py = (robot.py + second * robot.vy) % height
+        new_robots.append(robot._replace(px=px, py=py))
+    return new_robots
 
-    return safety_factor
+
+def check_pattern(robots, second):
+    for r in range(height):
+        same_row = sum(1 if robot.py == r else 0 for robot in robots)
+        if same_row >= 20:
+            return height, second
+    for c in range(width):
+        same_col = sum(1 if robot.px == c else 0 for robot in robots)
+        if same_col >= 20:
+            return width, second
+    return None
+
+
+def find_christmas_tree(robots):
+    patterns = []
+    # Iterate through first 100 seconds to find the 2 patterns
+    for i in range(100):
+        robots = calculate_positions(robots, 1)
+        pattern = check_pattern(robots, i + 1)
+        if pattern is not None:
+            patterns.append(pattern)
+            if len(patterns) == 2:
+                break
+
+    # Calculate seconds when christmas tree appears using CRT
+    return chinese_remainder(patterns)
 
 
 if __name__ == '__main__':
     robots = [Robot(*tuple(map(int, re.findall(r'(-?\d+)', line)))) for line in lines]
 
-    safety_factor = simulate_robots(robots, False)
+    safety_factor = calculate_safety_factor(calculate_positions(robots, 100))
     print(safety_factor)
+
+    christmas_tree_seconds = find_christmas_tree(robots)
+    print(christmas_tree_seconds)
