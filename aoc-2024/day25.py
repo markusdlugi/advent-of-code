997f86d407420e3bb6ac0e6c023b99fab5a7cc20
Markus Dlugi <dlugi.markus@gmail.com>
2024-12-25 12:31:51 +0100

    Improve day 25

--- aoc-2024/day25.py
+++ aoc-2024/day25.py
@@ -1,58 +1,19 @@
 from itertools import product
 
-lines = [line for line in open("input/25.txt")]
+schematics = [schematic.splitlines() for schematic in open("input/25.txt").read().split("\n\n")]
 
 keys = []
 locks = []
-current = set()
-start_r = 0
-for r, line in enumerate(lines):
-    for c, word in enumerate(line):
-        if line == "\n":
-            start_r = r + 1
-            if (0, 0) in current:
-                locks.append(current.copy())
-            else:
-                keys.append(current.copy())
-            current.clear()
-        elif word == "#":
-            current.add((r - start_r, c))
-
-if (0, 0) in current:
-    locks.append(current)
-else:
-    keys.append(current)
-
-lock_heights = []
-for l in locks:
-    heights = []
-    for c in range(0, 5):
-        for r in range(1, 6):
-            if not (r, c) in l:
-                heights.append(r - 1)
-                break
-        else:
-            heights.append(5)
-    lock_heights.append(tuple(heights))
-
-key_heights = []
-for k in keys:
-    heights = []
-    for c in range(0, 5):
-        for r in range(5, 0, -1):
-            if not (r, c) in k:
-                heights.append(5 - r)
-                break
-        else:
-            heights.append(5)
-    key_heights.append(tuple(heights))
-
-fits = []
-for key, lock in product(key_heights, lock_heights):
-    for c in range(0, 5):
-        if key[c] + lock[c] > 5:
-            break
+for schematic in schematics:
+    current = set()
+    for r, line in enumerate(schematic):
+        for c, word in enumerate(line):
+            if word == "#":
+                current.add((r, c))
+    if schematic[0][0] == "#":
+        locks.append(current)
     else:
-        fits.append((key, lock))
+        keys.append(current)
 
-print(len(fits))
+fits = sum(1 for key, lock in product(keys, locks) if not key.intersection(lock))
+print(fits)
